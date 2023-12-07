7b22fc84ade1c556107e29019d75235144d96bcf
mecklenborg <mecklenborg@indeed.com>
2023-12-06 18:24:48 -0600

    2023 Day 06, now featuring the one and only Quadratic Formula (TM), that thing you swore in high school that you would never need to use again

--- day06/solution.py
+++ day06/solution.py
@@ -4,25 +4,29 @@ import math
 with open('input.txt') as input:
     lines = input.read().splitlines()
 
+def quad_solver(t, d) -> tuple:
+    x1 = (t - math.sqrt(t**2 - 4*d))/2
+    x2 = (t + math.sqrt(t**2 - 4*d))/2
+    return (x1, x2)
+
+# Part 1
 wins = []
 
 times = [int(t) for t in re.findall(r'\d+', lines[0])]
 dists = [int(d) for d in re.findall(r'\d+', lines[1])]
 
-for idx, time in enumerate(times):
-    wins.append(0)
-    for t in range(1, time):
-        if t * (time - t) > dists[idx]:
-            wins[idx] +=1
+for time, dist in zip(times, dists):
+    x1, x2 = quad_solver(time, dist)
+    min_hold, max_hold = math.floor(x1 + 1), math.ceil(x2 - 1)
+    wins.append(max_hold - min_hold + 1)
 
+
+# Part 2
 time = int(''.join(re.findall(r'\d+', lines[0])))
 dist = int(''.join(re.findall(r'\d+', lines[1])))
 
-wins_2 = 0
-
-for t in range(1, time):
-    if t * (time - t) > dist:
-        wins_2 +=1
+x1, x2 = quad_solver(time, dist)
+min_hold, max_hold = math.floor(x1 + 1), math.ceil(x2 - 1)
 
 print(f'Part 1: {math.prod(wins)}')
-print(f'Part 2: {wins_2}')
+print(f'Part 2: {max_hold - min_hold + 1}')
