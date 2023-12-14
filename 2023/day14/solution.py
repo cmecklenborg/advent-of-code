fab3240f9ead4f500d3aeef3f5c1ea23eea2455a
mecklenborg <mecklenborg@indeed.com>
2023-12-14 17:25:46 -0600

    2023 Day 14, Part 1 - working solution

--- 2023/day14/solution.py
+++ 2023/day14/solution.py
@@ -1,6 +1,4 @@
-from collections import defaultdict
-
-file = 'sample.txt'
+file = 'input.txt'
 with open(file) as input:
     lines = input.read().splitlines()
 
@@ -14,17 +12,20 @@ for y, line in enumerate(lines):
             cube_rocks.append(coords)
         elif c == 'O':
             round_rocks.append(coords)
-            # prev_round_rocks = [c[0] for c in round_rocks if c[1] == y]
-            # prev_cube_rocks = [c[0] for c in cube_rocks if c[1] == y]
-            # new_x = min([x, len(prev_round_rocks)] + prev_cube_rocks)
-            # round_rocks.append((new_x, y))
 
-
-# for col in range(0, len(lines[0])):
-for col in range(0, 1):
+load = 0
+max_x = len(lines[0])
+max_y = len(lines)
+for col in range(0, max_x):
     round_rows = [r[1] for r in round_rocks if r[0] == col]
     cube_rows = [c[1] for c in cube_rocks if c[0] == col]
-    print(f'Round rows: {round_rows}')
-    print(f'Cube rows: {cube_rows}')
+    cube_rows.insert(0, -1)
 
-print(round_rocks)
+    for ridx in range(1, len(cube_rows)):
+        round_count = sum([1 for rr in round_rows if rr > cube_rows[ridx-1] and rr < cube_rows[ridx]])
+        round_rows = round_rows[round_count:]
+        load += sum([max_y - 1 - x - cube_rows[ridx-1] for x in range(0, round_count)])
+
+    load += sum([max_y - 1 - x - cube_rows[-1] for x in range(0, len(round_rows))])
+    
+print(load)
