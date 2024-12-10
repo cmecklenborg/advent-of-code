fa6bcdcf332373a4e896a296fdd77f140a1901cc
Christine Mecklenborg <christine.mecklenborg@gmail.com>
2024-12-09 19:34:07 -0600

    2024 Day 3 - Mo' regex mo' problems

--- 2024/day03/solution.py
+++ 2024/day03/solution.py
@@ -17,4 +17,28 @@ def part_1():
     return result
 
 
+def part_2():
+    result = 0
+    do = True
+    for line in lines:
+        muls = re.findall(r"mul\(\d+,\d+\)", line)
+        dos = re.findall(r"do[^n].*?mul\(\d+,\d+\)", line)
+        donts = re.findall(r"don't.*?mul\(\d+,\d+\)", line)
+
+        for mul in muls:
+            if len(dos) > 0 and mul in dos[0]:
+                do = True
+                dos.pop(0)
+            elif len(donts) > 0 and mul in donts[0]:
+                do = False
+                donts.pop(0)
+
+            if do:
+                x, y = re.findall(r"\d+", mul)
+                result += int(x) * int(y)
+
+    return result
+
+
 print(f"Part 1: {part_1()}")
+print(f"Part 2: {part_2()}")
