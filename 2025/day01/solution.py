195337210a4065f7cc3443bcd9e8fc42bcd9ec5a
Christine Mecklenborg <christine.mecklenborg@gmail.com>
2025-12-01 20:55:07 -0800

    2025 Day 1, Part 2 - Thanks, I hate it

--- 2025/day01/solution.py
+++ 2025/day01/solution.py
@@ -17,5 +17,18 @@ def part_1():
     return z
 
 
+def part_2():
+    x = 50
+    z = 0
+    for line in lines:
+        d = int(line[1:])
+        sign = -1 if line[0] == "R" else 1
+        for _ in range(d):
+            x += sign
+            if x % 100 == 0:
+                z += 1
+    return z
+
+
 print(f"Part 1: {part_1()}")
-# print(f"Part 2: {part_2()}")
+print(f"Part 2: {part_2()}")
