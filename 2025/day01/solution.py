9964e502bceb0ce6923940892ef53b04e66f0de9
Christine Mecklenborg <christine.mecklenborg@gmail.com>
2025-12-01 20:36:32 -0800

    2025 Day 1, Part 1

---
+++ 2025/day01/solution.py
@@ -0,0 +1,21 @@
+file = "input.txt"
+
+with open(file) as input:
+    lines = input.read().splitlines()
+
+
+def part_1():
+    x = 50
+    z = 0
+
+    for line in lines:
+        d = int(line[1:])
+        x += d * (-1 if line[0] == "R" else 1)
+        if x % 100 == 0:
+            z += 1
+
+    return z
+
+
+print(f"Part 1: {part_1()}")
+# print(f"Part 2: {part_2()}")
